ac6bf5c23c78b206b0d54d03d078cc8a5c1cf11e
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-05 19:18:29 -0500

    Create  Drawing Book.py

---
+++ Drawing Book.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    frontflip=math.floor(p/2)
+    backflip=math.floor((n/2)-frontflip)
+    return min(frontflip, backflip)
+    #the min options depend whether its quicker to turn pages from the left or right
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
