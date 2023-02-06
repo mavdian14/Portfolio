74e2c95023feed056206ae532150951fd4b4fb91
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-05 19:27:30 -0500

    Create Viral Advertising.py

---
+++ Viral Advertising.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    num=5
+    totnum=0
+    for day in range(n):
+        numlike=num//2
+        totnum += numlike
+        num=numlike*3
+    
+    return totnum
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
