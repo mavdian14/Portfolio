32c8adeeb65d2bd3739a5a53a251c1cd834c166c
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-05 19:59:09 -0500

    Create Equalize the Array.py

---
+++ Equalize the Array.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    return (len(arr) -arr.count(max(set(arr) ,key = arr.count)))
+#the 2nd function: the max function finds with the integer with the most occurences in the set. Then arr.count finds how many occurences there are.
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
