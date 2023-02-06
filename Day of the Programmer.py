0a420630c5b893381bd100351a486d0d2a8838f2
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-05 19:01:09 -0500

    Create Day of the Programmer.py

---
+++ Day of the Programmer.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# % inside a string c lets you format any string s into it with "%s" % s
+
+def dayOfProgrammer(year):
+    if year == 1918:
+        return "26.09.1918"
+    #leap year condition for all calendars
+    elif (year < 1917 and year % 4 == 0) or (year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))):
+        return "12.09.%s" % year
+    else:
+        return "13.09.%s" % year
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
