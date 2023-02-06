2c8620482eb97a9b3d6f1ccd450f81045c721290
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-05 19:30:40 -0500

    Create Circular Array Rotation.py

---
+++ Circular Array Rotation.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'circularArrayRotation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER k
+#  3. INTEGER_ARRAY queries
+#
+
+def circularArrayRotation(a, k, queries):
+    n = len(a)
+    #n-k gives the first index of a after k circular rotations.n-k+q will give us the last index of item required for the ans. If this answer exceeds n-1 we will be out of the bounds of the range, thus, we need mod n of in that case.
+    ans=[]
+    for q in queries:
+        ans.append(a[(n-k+q)%n])
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
