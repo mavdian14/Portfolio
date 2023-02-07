58cec8988338213b67819c9954589b1596d7793b
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-06 20:15:11 -0500

    Create Quicksort 1 - Partition.py

---
+++ Quicksort 1 - Partition.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    left=[]
+    right=[]
+    equal=[]
+    pivot=arr[0]
+    
+    for i in arr:
+        if i < pivot:
+            left.append(i)
+        elif i == pivot:
+            equal.append(i)
+        elif i > pivot:
+            right.append(i)
+    return left + equal + right
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
