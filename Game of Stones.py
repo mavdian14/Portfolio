288b2a8754ad1b7d8b19847d34961fd7a31590c3
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-02-06 21:02:02 -0500

    Create Game of Stones.py

---
+++ Game of Stones.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameOfStones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER n as parameter.
+#
+
+def gameOfStones(n):
+    return 'Second' if n%7<2 else 'First'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = gameOfStones(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
