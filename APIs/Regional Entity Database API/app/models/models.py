122170ab3b212d5f901b3a37970c2aebe7f50568
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-09-29 14:51:36 -0400

    Create models.py

---
+++ APIs/Regional Entity Database API/app/models/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+
+
+class ParcelInfo(BaseModel):
+    parcels: list
+    buildings: list
+    units: list
+
+class FindLatestUpdate(BaseModel):
+    last_update: date
+
+class FilterParcelCounts(BaseModel):
+    building_use: str
+    count: int
+
+class FilterParcelIds(BaseModel):
+    parcel_id: str
+
+class LegalEntity(BaseModel):
+    legal_entity_id: int
+    legal_entity_address: Optional[str]
+    legal_entity_name: Optional[str]
+    legal_entity_secondary_name: Optional[str]
+    address_id: int
