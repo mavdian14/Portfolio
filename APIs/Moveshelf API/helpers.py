88a14374e5078eeab7240e5882fc87cfe2a49e70
mavdian14 <124536799+mavdian14@users.noreply.github.com>
2023-10-05 21:25:15 -0400

    Update helpers.py

--- APIs/Moveshelf API/helpers.py
+++ APIs/Moveshelf API/helpers.py
@@ -4,20 +4,27 @@ import requests
 import json
 import pandas as pd
 
+#return a logger that is the root logger of 'moveshelf-api' hierarchy
 logger = logging.getLogger('moveshelf-api')
 
 class Helpers:
 
+    #returns a static method for a timeToCycle    
     @staticmethod
     def timeToCycle(series):
         series = (series - series.min()) / (series.max() - series.min())
         return series * 100
 
+    
     @staticmethod
     def download_file(url, local_filename):
+        #with to replace try-catch block more concisely
         with requests.get(url, stream=True) as r:
+            #raise_for_status() returns an HTTPError object if an error has occurred
             r.raise_for_status()
+            #open "local_filename"
             with open(local_filename, 'wb') as f:
+                # iter.content() iterates over the response content
                 for chunk in r.iter_content(chunk_size=8192): 
                     if chunk: # filter out keep-alive new chunks
                         f.write(chunk)
@@ -27,6 +34,7 @@ class Helpers:
     @staticmethod
     def parseParametersFile(paramsFile):
         with open(paramsFile) as f:
+          #returm json object of paramsFile
           params = json.load(f)
     
         params_df = pd.DataFrame.from_dict(params['data'])
@@ -39,7 +47,8 @@ class Helpers:
     def parseEventFile(eventsFile):
         with open(eventsFile) as f:
           events = json.load(f)
-      
+
+        #return a df of dict events['events']
         return pd.DataFrame.from_dict(events['events'])
 
     @staticmethod
@@ -54,8 +63,11 @@ class Helpers:
         for d in data:
             url = d['previewDataUri']
             filename = d['originalFileName']
+            #check if dirpath path doesn't exist
             if not os.path.exists(dirpath):
+                #create dirpath directory recursively
                 os.makedirs(dirpath)
 
             if d['dataType'] != 'video' and d['dataType'] != 'motion':
+                #downloads url; optionally caches the result
                 Helpers.download_file(url, dirpath + '/' + filename)
